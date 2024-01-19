f141bfd2d4dc4af1d86a414103538b1f3c2f9be8
bogdanovsi <bogdanovsi@gmail.com>
2024-01-19 15:42:11 +0300

    1

--- C200/Lab1/myRect.cpp
+++ C200/Lab1/myRect.cpp
@@ -6,6 +6,28 @@ Rect::Rect(int left, int right, int top, int bottom)
     : m_left(left), m_right(right), m_top(top), m_bottom(bottom) {
     Normalize();
 }
+
+Rect::Rect()
+    : m_left(0), m_right(0), m_top(0), m_bottom(0) {
+}
+
+void Rect::InflateRect() {
+    m_left -= 1;
+    m_right += 1;
+    m_top -= 1;
+    m_bottom += 1;
+    Normalize();
+}
+
+void Rect::InflateRect(int increment) {
+    m_left -= increment;
+    m_right += increment;
+    m_top -= increment;
+    m_bottom += increment;
+    Normalize();
+}
+
+
 void Rect::InflateRect(int xIncrement, int yIncrement) {
     m_left -= xIncrement;
     m_right += xIncrement;
--- C200/Lab1/myRect.h
+++ C200/Lab1/myRect.h
@@ -7,10 +7,12 @@ public:
     int m_top;
     int m_bottom;
 
-   // Rect();  // Конструктор без параметров
+    Rect();  // Конструктор без параметров
     Rect(int left, int right, int top, int bottom);  // Конструктор с параметрами
     void InflateRect(int xIncrement, int yIncrement);  // Метод увеличения размеров прямоугольника
     void InflateRect(int xIncrement, int yIncrement, int xIncrement2, int yIncrement2);  // Перегруженный метод для увеличения размеров прямоугольника
+    void InflateRect(int increment);  // Перегруженный метод с одним аргументом
+    void InflateRect();
     void Normalize();  // Метод для проверки и коррекции "ненормализованного" прямоугольника
     void PrintRect() const;  // Метод для вывода прямоугольника в консоль
     void SetAll(int left, int right, int top, int bottom);  // Метод для установки значений private-переменных
@@ -20,9 +22,10 @@ public:
     int GetRight() const;
     int GetTop() const;
     int GetBottom() const;
-    Rect() : m_left(0), m_right(0), m_top(0), m_bottom(0) {}
+    
     Rect BoundingRectMethod(const Rect& rect) const;
 };
+
 
 Rect BoundingRect(const Rect& rect1, const Rect& rect2);
 Rect BoundingRect2(const Rect& rect1, const Rect& rect2);

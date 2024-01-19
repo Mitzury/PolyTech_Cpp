e908b91d51de4cbfabcfc6a6a50c642ece888747
bogdanovsi <bogdanovsi@gmail.com>
2024-01-19 11:44:44 +0300

    1

--- C200/Lab1/myRect.cpp
+++ C200/Lab1/myRect.cpp
@@ -1,1 +1,45 @@
 #include "myRect.h"
+#include <iostream>
+
+Rect::Rect()
+    : m_left(0), m_right(0), m_top(0), m_bottom(0) {
+}
+Rect::Rect(int left, int right, int top, int bottom)
+    : m_left(left), m_right(right), m_top(top), m_bottom(bottom) {
+    Normalize();
+}
+void Rect::InflateRect(int xIncrement, int yIncrement) {
+    m_left -= xIncrement;
+    m_right += xIncrement;
+    m_top -= yIncrement;
+    m_bottom += yIncrement;
+    Normalize();
+}
+void Rect::InflateRect(int xIncrement, int yIncrement, int xIncrement2, int yIncrement2) {
+    m_left -= xIncrement;
+    m_right += xIncrement2;
+    m_top -= yIncrement;
+    m_bottom += yIncrement2;
+    Normalize();
+}
+void Rect::Normalize() {
+    if (m_left > m_right) std::swap(m_left, m_right);
+    if (m_top > m_bottom) std::swap(m_top, m_bottom);
+}
+void Rect::PrintRect() const {
+    std::cout << "Rect: " << m_left << ", " << m_right << ", " << m_top << ", " << m_bottom << std::endl;
+}
+// Задание 3
+void Rect::SetAll(int left, int right, int top, int bottom) {
+    m_left = left;
+    m_right = right;
+    m_top = top;
+    m_bottom = bottom;
+    Normalize();
+}
+void Rect::GetAll(int& left, int& right, int& top, int& bottom) const {
+    left = m_left;
+    right = m_right;
+    top = m_top;
+    bottom = m_bottom;
+}
--- C200/Lab1/myRect.h
+++ C200/Lab1/myRect.h
@@ -1,6 +1,19 @@
 #pragma once
-class Rect{
+
+class Rect {
 public:
-	int m_left, m_right, m_top, m_bottom;
-	
+    int m_left;
+    int m_right;
+    int m_top;
+    int m_bottom;
+
+public:
+    Rect();  // Конструктор без параметров
+    Rect(int left, int right, int top, int bottom);  // Конструктор с параметрами
+    void InflateRect(int xIncrement, int yIncrement);  // Метод увеличения размеров прямоугольника
+    void InflateRect(int xIncrement, int yIncrement, int xIncrement2, int yIncrement2);  // Перегруженный метод для увеличения размеров прямоугольника
+    void Normalize();  // Метод для проверки и коррекции "ненормализованного" прямоугольника
+    void PrintRect() const;  // Метод для вывода прямоугольника в консоль
+    void SetAll(int left, int right, int top, int bottom);  // Метод для установки значений private-переменных
+    void GetAll(int& left, int& right, int& top, int& bottom) const;  // Метод для получения значений private-переменных
 };

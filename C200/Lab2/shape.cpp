7e82a44b3cd07278e84eea079b67048ed249c02d
bogdanovsi <bogdanovsi@gmail.com>
2024-02-02 17:54:57 +0300

    1

--- C200/Lab2/shape.cpp
+++ C200/Lab2/shape.cpp
@@ -1,5 +1,6 @@
 ﻿#include "shape.h"
 
+// Функция преобразования перечисления Color в строку
 const char* colorToString(Color c) {
     switch (c) {
     case RED:
@@ -13,31 +14,43 @@ const char* colorToString(Color c) {
     }
 }
 
+// Реализация методов базового класса Shape
+
+// Конструктор Shape, устанавливающий цвет
 Shape::Shape(Color c) : color(c) {}
 
+// Геттер для цвета
 Color Shape::getColor() const {
     return color;
 }
 
-void Shape::WhereAmI() const {
+// Невиртуальная функция, сообщающая о местоположении объекта Shape
+void Shape::WhereAmI() const { 
     std::cout << "Now I am in class Shape" << std::endl;
 }
 
+// Виртуальная функция, сообщающая о местоположении объекта Shape (может быть переопределена)
 void Shape::WhereAmIVirtual() const {
     std::cout << "Now I am in class Shape (virtual)" << std::endl;
 }
 
+// Виртуальный деструктор Shape
 Shape::~Shape() {
     std::cout << "Now I am in Shape's destructor!" << std::endl;
 
 }
 
+// Виртуальная функция для вывода информации о фигуре (может быть переопределена в производных классах)
 void Shape::printInfo() const {
     std::cout << "Color: " << colorToString(color) << std::endl;
 }
 
+// Реализация методов производного класса Rect
+
+// Конструктор Rect, устанавливающий цвет с использованием конструктора базового класса Shape
 Rect::Rect(Color c) : Shape(c) {}
 
+// Невиртуальная функция, сообщающая о местоположении объекта Rect
 void Rect::WhereAmI() const {
     std::cout << "Now I am in class Rect" << std::endl;
 
@@ -64,6 +77,9 @@ Circle::Circle(Color c) : Shape(c) {
     std::cout << "Constructing Circle" << std::endl;
 }
 
+// Реализация методов производного класса Circle
+
+// Конструктор Circle, устанавливающий цвет с использованием конструктора базового класса Shape
 Circle::Circle() : Shape(RED) {}
 
 Circle::Circle(const Rect& rect) : Shape(rect.getColor()) {}
@@ -73,14 +89,17 @@ void Circle::WhereAmI() const {
 
 }
 
+// Переопределенная виртуальная функция базового класса Shape
 void Circle::WhereAmIVirtual() const {
     std::cout << "Now I am in class Circle (virtual)" << std::endl;
 }
 
+// Переопределенная виртуальная функция для вывода информации о круге
 void Circle::printInfo() const {
     std::cout << "Circle of color: " << colorToString(color) << std::endl;
 }
 
+// Реализация чисто виртуальной функции Inflate для круга
 void Circle::Inflate(int factor) {
     std::cout << "Circle is inflating by a factor of " << factor << std::endl;
 }

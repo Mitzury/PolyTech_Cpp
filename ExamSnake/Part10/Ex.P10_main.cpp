56f3164f149b42f4b70fdb148acdf316b2b5af5f
bogdanovsi <bogdanovsi@gmail.com>
2024-02-04 15:48:39 +0300

    add's menu

--- ExamSnake/Part10/Ex.P10_main.cpp
+++ ExamSnake/Part10/Ex.P10_main.cpp
@@ -5,13 +5,35 @@
 
 using namespace std;
 
+// Функция для отображения главного меню
+void showMainMenu() {
+    system("cls");
+    std::cout << "1) Начало игры" << std::endl;
+    std::cout << "2) Настройка игры" << std::endl;
+    std::cout << "3) Топ" << std::endl;
+    std::cout << "4) Выход" << std::endl;
+}
+
 // Главная функция игры
 int main() {
         setlocale(LC_ALL, "ru");
 
-        Game game;
-        game.startGame();
-        game.delay = 2000; // Задержка в 250 миллисекунд между шагами игры
+        int choice;
+        showMainMenu();
+        std::cout << "Выберите пункт меню: ";
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            Game game;
+            game.startGame();
+            game.delay = 2000; // Задержка в 250 миллисекунд между шагами игры
+            break;
+        default:
+            std::cout << "Неправильный выбор." << std::endl;
+        case 4:
+            exit;
+        }
         return 0;
 }
 

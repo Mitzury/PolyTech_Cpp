947f76ef33d2ff880fd19c0d767cfe37941b56e4
bogdanovsi <bogdanovsi@gmail.com>
2024-02-16 10:44:35 +0300

    adds mystring

---
+++ C200/Lab4/MyString.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+class MyString {
+private:
+    char* str; // Указатель на массив символов 
+
+public:
+    MyString(const char* s = ""); // Конструктор, принимающий C-style строку (по умолчанию пустая строка)
+    MyString(const MyString& other); // Конструктор копирования
+    ~MyString(); // Деструктор
+    MyString& operator=(const MyString& other); // Оператор присваивания
+    bool operator==(const char* s) const; // Оператор сравнения с C-style строкой
+    bool operator>(const MyString& other) const; // Оператор "больше" для строк
+    bool operator<(const MyString& other) const; // Оператор "меньше" для строк
+    bool operator==(const MyString& other) const; // Оператор сравнения строк
+    friend std::ostream& operator<<(std::ostream& os, const MyString& str); // Перегруженный оператор вывода
+};

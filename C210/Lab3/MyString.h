8cd215f575ccce5ec555bcf5e2a6b96b96699683
bogdanovsi <bogdanovsi@gmail.com>
2024-04-04 08:55:36 +0300

    1

---
+++ C210/Lab3/MyString.h
@@ -0,0 +1,10 @@
+#include <string>
+
+class MyString {
+private:
+	std::string str;
+public:
+	MyString(const std::string& s) : str(s) {}
+	const std::string& get() const { return str; }
+	void set(const std::string& s) { str = s; }
+};

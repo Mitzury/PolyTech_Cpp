01f7b74d4a936429db3c0c15b4068fecb98477c8
bogdanovsi <bogdanovsi@gmail.com>
2024-04-04 09:18:36 +0300

    1

---
+++ C210/Lab3/Point.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <ostream>
+
+class Point {
+public:
+	double x, y;
+	Point() : x(0), y(0) {} // Конструктор по умолчанию
+	Point(double x_val, double y_val) : x(x_val), y(y_val) {} // Конструктор с параметрами
+	Point(int x_val = 0, int y_val = 0) : x(x_val), y(y_val) {}
+
+	// Перегрузка оператора << для вывода элементов типа Point
+	friend std::ostream& operator<<(std::ostream& os, const Point& point) {
+		os << "(" << point.x << ", " << point.y << ")";
+		return os;
+	}
+};
+
+// Перегрузка оператора << для вывода объектов типа Point
+std::ostream& operator<<(std::ostream& os, const Point* point) {
+	os << "x: " << point->x << ", y: " << point->y;
+	return os;
+}

1ecba9310be62f2ef076201e9d670011ceab8a9f
bogdanovsi <bogdanovsi@gmail.com>
2024-04-26 09:00:13 +0300

    1

--- C210/Lab2/MyStack2.h
+++ C210/Lab2/MyStack2.h
@@ -2,182 +2,131 @@
 #include <sstream>
 
 // Класс MyStack2
-
 template <typename T>
 class MyStack2 {
-private:
-    template<typename T>
-    class List {
-    private:
-        bool is_deleted = false;
-        // Вложенный класс node для представления элемента списка
-        class Node {
-        public:
-            T data;
-            // указатель на следующий элемент
-            Node* next;
-            // конструктор с параметром
-            Node(const T& data) : next(nullptr), data(data) {}
-            // конструктор без параметров
-            Node() : next(nullptr), data(nullptr) {}
-        };
-        // головной узел списка
-        Node head;
-    public:
-        // Конструктор по умолчанию
-        List() {
-            head = Node();
-        }
-        // Конструктор перемещения
-        List(List&& other) : head(other.head) {
+public:
+
+    MyStack2() {
+        head = Node();
+    }
+
+    MyStack2(MyStack2&& other) : head(std::move(other.head)) {
+        other.head = Node();
+    }
+
+    MyStack2& operator=(MyStack2&& other) {
+        if (this != &other) {
+            clear();
+
+            head = std::move(other.head);
             other.head = Node();
         }
-        // Оператор перемещения
-        List& operator=(List&& other) {
-            if (this != &other) {
-                clear();
-                head = other.head;
-                other.head = Node();
-            }
-            return *this;
+        return *this;
+    }
+
+    MyStack2(const MyStack2<T>& other) {
+        clear();
+        head = Node();
+        Node* current = other.head.next;
+        while (current != nullptr) {
+            AddToTail(current->data);
+            current = current->next;
         }
-        // Оператор копирования
-        List& operator=(const List& other) {
-            if (this != &other) {
-                clear();
-                Node* current = other.head.next;
-                while (current != nullptr) {
-                    AddToTail(current->data);
-                    current = current->next;
-                }
-            }
-            return *this;
-        }
-        // Метод для добавления элемента в конец списка
-        void AddToTail(const T& data) {
-            Node* newNode = new Node(data);
-            if (head.next == nullptr) {
-                head.next = newNode;
-            } else {
-                Node* current = head.next;
-                while (current->next != nullptr)
-                    current = current->next;
-                current->next = newNode;
-            }
-        }
-        // Метод для получения последнего элемента списка
-        T GetLast() {
-            Node* current = head.next;
-            while (current->next != nullptr)
-                current = current->next;
-            return current->data;
-        }
-        // Метод для удаления последнего элемента списка
-        void RemoveLast() {
-            if (head.next != nullptr)
-            {
-                Node* current = head.next;
-                while (current->next->next != nullptr)
-                    current = current->next;
-                Node* tmp = current->next;
-                current->next = nullptr;
-                delete tmp;
-            }
-        }
-        // Конструктор копирования
-        List(const List& other) {
+    }
+
+    ~MyStack2() {
+        if (!is_deleted) {
             clear();
-            Node* current = other.head;
+            is_deleted = true;
+        }
+    }
+
+    // Метод для добавления в стэк
+    void push(const T& value) {
+        AddToTail(value);
+    }
+
+    // Метод для получения объекта из стэка
+    T pop() {
+        if (empty()) {
+            throw std::underflow_error("Exception: Stack is empty");
+        }
+        T result = GetLast();
+        RemoveLast();
+        return result;
+    }
+
+    // Метод для определения наполненности стэка
+    bool empty() const {
+        return head.next == nullptr;
+    }
+
+    MyStack2<T>& operator=(const MyStack2<T>& other) {
+        if (this != &other) {
+            clear();
+
+            Node* current = other.head.next;
             while (current != nullptr) {
                 AddToTail(current->data);
                 current = current->next;
             }
         }
-        // Перегрузка оператора вывода в поток для списка
-        friend std::ostream& operator<<(std::ostream& os, const List& list) {
-            Node* current = list.head.next;
-            while (current != nullptr) {
-                os << current->data << ' ';
-                current = current->next;
-            }
-            os << std::endl;
-            return os;
-        }
-        // Перегрузка оператора ввода из потока для списка
-        friend std::istream& operator>>(std::istream& is, List& list) {
-            std::string str;
-            while (std::getline(is, str)) {
-                T c = T(str);
-                list.AddToTail(c);
-            }
-            return is;
-        }
-        // Метод для очистки списка
-        void clear() {
-            Node* current = head.next;
-            while (current != nullptr) {
-                Node* tmp = current;
-                current = current->next;
-                delete tmp;
-            }
-            head.next = nullptr;
-        }
-        // Метод для проверки, пуст ли список
-        bool empty() const {
-            return head.next == nullptr;
-        }
-        // Деструктор
-        ~List() {
-            if (!is_deleted) {
-                clear();
-                is_deleted = true;
-            }
-        }
+        return *this;
+    }
+
+private:
+    bool is_deleted = false;
+    class Node {
+    public:
+        T data;
+        Node* next;
+        Node(const T& data) : next(nullptr), data(data) {}
+        Node() : next(nullptr), data(nullptr) {}
     };
+    Node head;
 
-public:
-    MyStack2() {
-        m_data = List<T>();
-    }
-    MyStack2(MyStack2&& other) {
-        m_data = std::move(other.m_data);
-    }
-    MyStack2& operator=(MyStack2&& other) {
-        m_data = std::move(other.m_data);
-        return *this;
-    }
-    MyStack2(const MyStack2& other) {
-        m_data = other.m_data;
-    }
-    ~MyStack2() {
-        m_data.~List();
-    }
-    // Метод для добавления в стэк
-    void push(const T& value) {
-        m_data.AddToTail(value);
-    }
-    // Метод для получения объекта из стэка
-    T pop() {
-        if (m_data.empty()) {
-            throw std::underflow_error("Exception: Stack is empty");
+    void AddToTail(const T& data) {
+        Node* newNode = new Node(data);
+        if (head.next == nullptr) {
+            head.next = newNode;
         }
-        T result = m_data.GetLast();
-        m_data.RemoveLast();
-        return result;
-    }
-    // Метод для определения наполненности стэка
-    bool empty() const {
-        return m_data.empty();
-    }
-    MyStack2<T>& operator=(const MyStack2<T>& other) {
-        if (this != &other) {
-            m_data.clear();
-            m_data = other.m_data;
+        else {
+            Node* current = head.next;
+            while (current->next != nullptr)
+                current = current->next;
+            current->next = newNode;
         }
-        return *this;
     }
 
-    List<T> m_data;
+    T GetLast() {
+        Node* current = head.next;
+        while (current->next != nullptr)
+            current = current->next;
+        return current->data;
+    }
+
+    void RemoveLast() {
+        if (head.next != nullptr)
+        {
+            Node* current = head.next;
+            while (current->next->next != nullptr)
+                current = current->next;
+            Node* tmp = current->next;
+            current->next = nullptr;
+            delete tmp;
+        }
+    }
+
+    void clear() {
+        Node* current = head.next;
+        while (current != nullptr) {
+            Node* tmp = current;
+            current = current->next;
+            delete tmp;
+        }
+
+        head.next = nullptr;
+    }
 
     // Перегрузка оператора << для вывода значений в поток
     friend std::ostream& operator<<(std::ostream& os, const MyStack2<T>& stack) {
@@ -186,15 +135,22 @@ public:
             os << "MyStack2 is empty\n";
             return os;
         }
-        os << stack.m_data;
+
+        Node* current = stack.head.next;
+        while (current != nullptr) {
+            os << current->data << ' ';
+            current = current->next;
+        }
+        os << std::endl;
         return os;
     }
 
     // Перегрузка оператора >> для ввода значений из потока
     friend std::istream& operator>>(std::istream& is, MyStack2<T>& stack) {
-        T value;
-        while (is >> value) {
-            stack.push(value);
+        std::string str;
+        while (std::getline(is, str)) {
+            T c = T(str);
+            stack.AddToTail(c);
         }
         return is;
     }

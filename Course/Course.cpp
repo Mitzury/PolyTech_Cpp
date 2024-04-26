e386b401766f5914ce4bac1f2b874925d452bb04
bogdanovsi <bogdanovsi@gmail.com>
2024-04-26 15:03:40 +0300

    1

--- Course/Course.cpp
+++ Course/Course.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
-#include <string>
-#include "../C100/Lab5/other.h"
 
 using namespace std;
 
 int main () {
 
-	const int N = 5;
-	int ar1[] = { 1, 2, 3, 4, 5 };
-	int ar2[] = { 10, 5, 8, 2, 7 };
-
-	PrintArray(ar1, N);
-	PrintArray(ar2, N);
-	int min1 = Min(ar1, N);
-	int min2 = Min(ar2, N);
-
-	std::cout << "Min in ar1: " << min1 << std::endl;
-	std::cout << "Min in ar2: " << min2 << std::endl;
-
-	const char* str1 = "Hello";
-	const char* str2 = "World";
-	int cmpResult = MyStrCmp(str1, str2);
-	std::cout << "Comparison result: " << cmpResult << std::endl;
 
 return 0;
 }

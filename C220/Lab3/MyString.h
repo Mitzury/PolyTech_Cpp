275f5817c9462e636873bf8666376836cc172bb4
bogdanovsi <bogdanovsi@gmail.com>
2024-05-27 12:16:34 +0300

    1

--- C220/Lab3/MyString.h
+++ C220/Lab3/MyString.h
@@ -1,12 +1,11 @@
 #pragma once
 class MyString {
 	char* m_pStr;
-	static const char* err;
 public:
-	MyString();
-	/*explicit*/ MyString(const char*);
-	MyString(const MyString&);
-	MyString(MyString&& in);
+	MyString();						// Конструктор по умолчанию, создает объект без строки
+	MyString(const char*);			// Конструктор, создающий объект с заданной строкой
+	MyString(const MyString&);		// Конструктор копирования, создает копию объекта существующего объекта
+	MyString(MyString&& in);		// Конструктор перемещения, создает новый объект, заимствуя ресурсы у существующего
 	~MyString();
 	const char* GetString() const;
 	void SetNewString(const char*);
